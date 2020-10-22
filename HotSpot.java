c2fb225fecd7cb3c19c8a90c76726eb987a3f760
humey <humeey44@gmail.com>
2020-10-22 12:07:51 +1100

    Add files via upload

---
+++ HotSpot.java
@@ -0,0 +1,31 @@
+// Comp2230 - Algorithm Assignment
+// Created by - Keeylan Hume C3320396
+// Hotspot class - Stores all relevant information about the hotspots
+//                 That have been read in from the input file
+
+public class HotSpot {
+
+    int hotSpotID;
+    float x, y;
+
+    public HotSpot(int hotSpotID, float x, float y) {
+
+        this.hotSpotID = hotSpotID;
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public int getHotSpotID() {
+        return hotSpotID;
+    }
+
+}
---
+++ Station.java
@@ -0,0 +1,35 @@
+// Comp2230 - Algorithm Assignment
+// Created by - Keeylan Hume C3320396
+// Station class - Stores all relevant information about the stations
+
+import java.util.ArrayList;
+
+public class Station {
+
+    private float x, y;
+    private int stationID;
+    private ArrayList<HotSpot> servicedHotspots;
+
+    public Station(float x, float y, ArrayList<HotSpot> servicedHotspots, int stationID) {
+        this.x = x;
+        this.y = y;
+        this.servicedHotspots = servicedHotspots;
+        this.stationID = stationID;
+    }
+
+    public String getHotSpots() {
+        String hotSpots = "";
+
+        for(HotSpot h : servicedHotspots) {
+
+            hotSpots = hotSpots.concat(Integer.toString(h.getHotSpotID()));
+
+            if(servicedHotspots.indexOf(h) != (servicedHotspots.size() - 1)) {
+
+                hotSpots = hotSpots.concat(",");
+
+            }
+        }
+        return hotSpots;
+    }
+}
